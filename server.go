27d125ea0780e998f96bd410bedce52a8db1542f
Avin Ben-Shemesh <avinbrown.focus@icloud.com>
2023-03-05 20:15:21 -0500

    added more comments for better understanding.

--- server.go
+++ server.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"net/http"
 )
+
+// *** the client is the web browser ***
 func handler(writer http.ResponseWriter, request *http.Request) {
     // will write back to the client -> localhost:8080 using the writter argument
 //    fmt.Fprintf(writer, "hellooooooooo world,")
@@ -16,5 +18,6 @@ func main() {
         // will be called for any incoming request that doesnt match a more specific URL path.
             // ex google.com/images -> in this case the path is "images" the domain name is "google.com"
 	http.HandleFunc("/", handler)
+    // sets up the HTTP SERVER to listen on Port 8080 of the Local Machine -> localhost:
 	http.ListenAndServe(":8080", nil)
 }
