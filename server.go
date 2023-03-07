d916c1b6e3dcac07c0d59cb1dca23266d71a4ee9
Avin Ben-Shemesh <avinbrown.focus@icloud.com>
2023-03-07 15:02:39 -0500

    no import.

--- server.go
+++ server.go
@@ -3,55 +3,35 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"reflect"
+	"runtime"
 )
 
-type Myhandler struct {}
+type helloHandler struct{}
 
-func chainGang(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Chain Chain"))
+func (h *helloHandler) ServeHTTP(writer http.ResponseWriter, request *http.Request) {
+	writer.Write([]byte("Does this work..."))
 }
 
-func hello2(w http.ResponseWriter, r *http.Request)  {
-    w.Write([]byte("Yelllllllooooow"))
+func hello2(writer http.ResponseWriter, req *http.Request) {
+	writer.Write([]byte("hello2"))
 }
-
-// log -> creartes a log that tell the developer that a handler function with the name
-    // name is being requested to the client using closure.
 func log(h http.HandlerFunc) http.HandlerFunc {
-    return func(writer http.ResponseWriter, request *http.Request) {
-        fmt.Printf("Handler %T was called", h)
-        h(writer,request)
-    }
+	return func(writer http.ResponseWriter, request *http.Request) {
+		name := runtime.FuncForPC(reflect.ValueOf(h).Pointer()).Name()
+		fmt.Printf("The handler %T was called\n", name)
+		// the return handlerFunc
+		h(writer, request)
+	}
 }
 
-type helloHand struct {}
-
-func (h *helloHand) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("A new Hello"))
-}
-
-type worldHand struct {}
-
-func (h *worldHand) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("A new world"))
-}
-func (h *Myhandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("helloooooo"))
-}
 func main() {
-	fmt.Printf("Server statring....\n")
+	hello := hello2
 
-
-	hello := helloHand{}
-	world := worldHand{}
-	// no url matchung needed so any path will be a valid request.
 	server := http.Server{
 		Addr: "127.0.0.1:8080",
 	}
-    // the log must be chain together in order for the log to work
-	http.HandleFunc("/hello2", log(hello2))
-	http.Handle("/hello", &hello)
-	http.Handle("/world", &world)
-	// starts the server listens for request on a specific addy in this case 8080 local host
+	fmt.Printf("starting server....")
+	http.HandleFunc("/hello2", log(hello))
 	server.ListenAndServe()
 }
