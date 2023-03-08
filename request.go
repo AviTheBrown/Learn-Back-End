869bdc3eeeee5b335e21eaa1a4407e7b6be326d5
Avin Ben-Shemesh <avinbrown.focus@icloud.com>
2023-03-07 22:35:50 -0500

    Learned about Form aand differnce between r.Form and r.PostForm methods.

--- request.go
+++ request.go
@@ -8,17 +8,20 @@ import (
 func headers(writer http.ResponseWriter, req *http.Request) {
     // this retreives information from the Head of the request
     // there should be no whitespace in the string arg
-	head := req.Header["Accept-Encoding"]
+    fmt.Fprintf(writer, "this is the header\n")
+//	head := req.Header["Accept-Encoding"]
     // or
-    head2 := req.Header.Get("Accept-Encoding")
+    head2 := req.Header.Get("Accept-Language")
 
     // prints a map of string
-    fmt.Fprintln(writer, head)
+//    fmt.Fprintln(writer, head)
     // prints a comma-delimited list of values
     writer.Write([]byte(head2))
 }
 
 func body(writer http.ResponseWriter, req *http.Request)  {
+    fmt.Fprintf(writer, "this is the body\n")
+
     // creates a var that represents the length of the message body in bytes,
     // not the header of the request.
     len := req.ContentLength
@@ -32,15 +35,21 @@ func body(writer http.ResponseWriter, req *http.Request)  {
 }
 
 func process(writer http.ResponseWriter, req *http.Request)  {
+    fmt.Fprintf(writer, "this is the process\n")
+
     req.ParseForm()
+    // this only parses and retireves the key-value pair of the form and omits the
+    // URL key-value pairs.
+    fmt.Fprintln(writer, req.PostForm)
+    // prints both the URL and form KV pairs
     fmt.Fprintln(writer, req.Form)
 }
 func main() {
 	server := http.Server{
 		Addr: "127.0.0.1:8080",
 	}
-//	http.HandleFunc("/header", headers)
-//    http.HandleFunc("/body", body)
+	http.HandleFunc("/header", headers)
+    http.HandleFunc("/body", body)
     http.HandleFunc("/process", process)
     fmt.Printf("Starting server.....")
 	server.ListenAndServe()
