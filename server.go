56ea10f287c3ca98d40c715dcf3f1958067b4eb7
Avin Ben-Shemesh <avinbrown.focus@icloud.com>
2023-03-08 23:56:29 -0500

    testing

--- server.go
+++ server.go
@@ -16,14 +16,14 @@ func (h *helloHandler) ServeHTTP(writer http.ResponseWriter, request *http.Reque
 func hello2(writer http.ResponseWriter, req *http.Request) {
 	writer.Write([]byte("hello2"))
 }
-func log(h http.HandlerFunc) http.HandlerFunc {
-	return func(writer http.ResponseWriter, request *http.Request) {
-		name := runtime.FuncForPC(reflect.ValueOf(h).Pointer()).Name()
-		fmt.Printf("The handler %T was called\n", name)
-		// the return handlerFunc
-		h(writer, request)
-	}
-}
+//func log(h http.HandlerFunc) http.HandlerFunc {
+//	return func(writer http.ResponseWriter, request *http.Request) {
+//		name := runtime.FuncForPC(reflect.ValueOf(h).Pointer()).Name()
+//		fmt.Printf("The handler %T was called\n", name)
+//		// the return handlerFunc
+//		h(writer, request)
+//	}
+//}
 
 func main() {
 	hello := hello2
