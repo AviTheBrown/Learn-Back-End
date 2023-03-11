6b61c4c891436e2414424dcdb3eeb5cc06f5171d
Avin Ben-Shemesh <avinbrown.focus@icloud.com>
2023-03-10 19:52:59 -0500

    changed method from to show cookies in browser

--- cookies.go
+++ cookies.go
@@ -3,28 +3,33 @@ import (
     "fmt"
 "net/http"
 )
-func setCookie (h http.ResponseWriter, r *http.Request) {
-    cookie1 := http.Cookie{
-        Name: "Cookie 1",
-        Value: "value 1",
+func setCookie(w http.ResponseWriter, r *http.Request){
+    c1 := http.Cookie {
+        Name: "cookie1",
+        Value: "My cookie",
+        HttpOnly: true,
     }
-    cookie2 := http.Cookie{
-        Name: "Cookie 2",
-        Value: "value 2",
+    c2 := http.Cookie {
+        Name: "cookie2",
+        Value: "second cookie value",
+        HttpOnly: true,
     }
-    http.SetCookie(h, &cookie1)
-    http.SetCookie(h, &cookie2)
-    fmt.Fprintln(h, "cookie cookie2222")
-
+    http.SetCookie(w, &c1)
+    http.SetCookie(w, &c2)
 }
 
-func getCookie(w http.ResponseWriter, r *http.Request)  {
-    h := r.Cookies()
-    for _, cookie := range h {
-        fmt.Fprintf(w, "this is the name from cookie", cookie.Name)
+func getCookie(w http.ResponseWriter, r *http.Request) {
+    c1 , err := r.Cookie("cookie1")
+    if err != nil {
+        fmt.Fprintln(w, "cookie can not be found")
     }
-    fmt.Fprintln(w, "cookie cookie")
+    cs := r.Cookies()
+    fmt.Fprintln(w, c1)
+    fmt.Fprintln(w, cs)
 }
+
+
+
 func main() {
 
     server := http.Server{
