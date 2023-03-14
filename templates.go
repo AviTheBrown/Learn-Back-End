bb22f0c87fdea9d2714a9a689642d644abd75120
Avin Ben-Shemesh <avinbrown.focus@icloud.com>
2023-03-14 13:56:34 -0400

    added t1 and t2 for templating examples.

--- templates.go
+++ templates.go
@@ -4,15 +4,16 @@ package main
 import (
 	"net/http"
 	"html/template"
-	"math/rand"
-	"time"
+	"fmt"
 )
 
 func main() {
+	// creates a new server instance
 	server := http.Server{
 		Addr: "127.0.0.1:8080",
 	}
-
+	// creates a new handle function the that
+	// invokes when using the "/parse" path
 	http.HandleFunc("/parse", processTempl)
 	fmt.Println("Server staring...")
 	server.ListenAndServe()
@@ -20,9 +21,12 @@ func main() {
 
 
 func processTempl(w http.ResponseWriter, r *http.Request){
-	t, _ := template.ParseFiles("tmpl.html")
+	// creates a template that parses a file named "tmpl"
+	// there is no error handling
+	template, _ := template.ParseFiles("t1.html", "t2.html")
 
-	rand.Seed(time.Now().Unix())
-	t.Execute(w, rand.Intn(10) > 5)
-
-}
+	// creates the template/templatr enginie that will write
+	// to the client according to the action in the file that
+	// was passed.
+	template.Execute(w,"Hello World!")
+	}
