f7e8a76943c9b47e70c1e7626b74ab60e4de6240
Avin Ben-Shemesh <avinbrown.focus@icloud.com>
2023-03-15 00:01:41 -0400

    created template in template.

--- templates.go
+++ templates.go
@@ -1,32 +1,39 @@
-
 package main
 
 import (
-	"net/http"
-	"html/template"
-	"fmt"
+    "html/template"
+    "net/http"
+    "time"
 )
 
-func main() {
-	// creates a new server instance
-	server := http.Server{
-		Addr: "127.0.0.1:8080",
-	}
-	// creates a new handle function the that
-	// invokes when using the "/parse" path
-	http.HandleFunc("/parse", processTempl)
-	fmt.Println("Server staring...")
-	server.ListenAndServe()
+func formatData (t time.Time) string {
+    layout := ""
+    return t.Format(layout)
 }
+func process2(w http.ResponseWriter, r *http.Request) {
+    // creates a key value pair { map[stirng][function] }
+    // that will be called once the template is executed.
+    funcMap := template.FuncMap{ "fdate": formatData }
+    // creates a new template .New("tmpl.html")
+    // and adds the string function map (funcMap)
+    t := template.New("tmpl.html").Funcs(funcMap)
 
+    // parsees the template.
+    t, _ = t.ParseFiles("tmpl.html")
 
-func processTempl(w http.ResponseWriter, r *http.Request){
-	// creates a template that parses a file named "tmpl"
-	// there is no error handling
-	template, _ := template.ParseFiles("t1.html", "t2.html")
+    // executes the template engine.
+//    By default, the Execute() method will use the time.Time object that was passed to it
+//    as the "dot" in the template,
+//    which means that it will be available to the template as the . variable.
+//    When you pass time.Now() as the argument to Execute(), it sets the {{.}} variable to
+//    the current time as a time.Time object.
+    t.Execute(w, time.Now())
 
-	// creates the template/templatr enginie that will write
-	// to the client according to the action in the file that
-	// was passed.
-	template.Execute(w,"Hello World!")
-	}
+}
+func main() {
+    server := http.Server{
+        Addr: "127.0.0.1:8080",
+    }
+    http.HandleFunc("/parse", process2)
+    server.ListenAndServe()
+}
